30086bc6400332079cb9f4955c1008d21f8febab
Yasyf Mohamedali <yasyfm@gmail.com>
2014-11-23 19:16:12 -0500

    Fix total weight calculation

--- helpers/calculators.py
+++ helpers/calculators.py
@@ -23,21 +23,18 @@ def calculate_averages(number, assignments, categories):
 
 def calculate_summed_totals(averages, assignments, categories):
   summed_totals = []
-  total_weight = sum([x[1] for x in categories.values()])
+  total_weight = sum([x[1] for x in map(lambda x: categories[x], averages.keys())])
 
   total_total = 0
   for k, average in averages.items():
     category = categories[k]
     weight = float(category[1]) / total_weight
-    avg = sum([x[0] for x in average]) / (len(average) or 1)
+    avg = sum([x[0]*x[1] for x in average]) / sum([x[1] for x in average])
     items = [clean_assignment(a) for a in assignments if a['categoryId'] == k]
     items = filter(lambda x: x, items)
     total = {'name': category[0], 'avg': avg, 'weight': weight, 'assignments': items}
     summed_totals.append(total)
     total_total += avg * weight
-
-  # values = sum(averages.values(), [])
-  # total_total = sum([x[0]*x[1] for x in values]) / (sum([x[1] for x in values]) or 1)
 
   summed_totals.insert(0, {'name': 'Weighted Total', 'avg': total_total})
 
